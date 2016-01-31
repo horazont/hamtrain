a6b7ffa62833564074fb102b4fee74206cd8fcc4
Jonas Wielicki <j.wielicki@sotecware.net>
2016-01-31 11:15:39 +0100

    fix bugs, add more query types, use readline within a question

--- hamtrain.py
+++ hamtrain.py
@@ -2,6 +2,7 @@
 import math
 import random
 import re
+import readline
 
 
 RNG = random.Random(random.SystemRandom().getrandbits(32))
@@ -24,6 +25,7 @@ PREFIXES = {
 }
 
 REV_PREFIXES = {
+    0: "",
     1: "k",
     2: "M",
     3: "G",
@@ -52,9 +54,9 @@ def parse_frequency_range(s):
             parse_frequency(match.group("end")))
 
 
-def format_frequency(f):
+def format_frequency(f, wide=True):
     order = math.floor(math.log(f, 10) / 3)-1
-    if f / (10**(order*3)) >= 3000:
+    if f / (10**(order*3)) >= 3000 or not wide:
         order += 1
 
     f_scaled = f / (10**(order*3))
@@ -101,9 +103,9 @@ def build_fullband_index(db):
         if class_ == "E":
             continue
         try:
-            start, end = fullband[λ]
+            (start, end), curr_bw = fullband[λ]
         except KeyError:
-            fullband[λ] = fs
+            fullband[λ] = fs, bw
         else:
             if end == fs[0]:
                 end = fs[1]
@@ -111,21 +113,43 @@ def build_fullband_index(db):
                 start = fs[0]
             else:
                 raise ValueError("incoherent band data")
-            fullband[λ] = start, end
+            if bw != curr_bw:
+                curr_bw = None
+            fullband[λ] = (start, end), curr_bw
 
     return fullband
 
 
-with open("db", "r") as infile:
-    db = list(read_database(infile))
+def build_classband_index(db):
+    classbands = {}
+    for class_, λ, *_ in db:
+        classbands.setdefault(class_, set()).add(λ)
+
+    return classbands
+
+try:
+    with open("db", "r") as infile:
+        db = list(read_database(infile))
+except FileNotFoundError:
+    import sys
+    print("copy db_master to db and remove everything you don’t want to learn")
+    sys.exit(1)
 
 
 fullband_index = build_fullband_index(db)
+classbands_index = build_classband_index(db)
 
 
-def query(prompt, parser=str):
+def query(prompt, parser=str, subline=None):
+    readline.clear_history()
+
     while True:
+        if subline is not None:
+            print()
+            print(" ({})".format(subline), end="\r\x1b[A")
         value = input(prompt)
+        if subline is not None:
+            print("\x1b[K", end="")
         if not value:
             return None
 
@@ -136,13 +160,20 @@ def query(prompt, parser=str):
             continue
 
 
-def print_eval(is_correct, actual_value):
+def print_eval(is_correct, actual_value, missing=None, surplus=None):
     if is_correct:
         print("correct!\n")
     else:
-        print("incorrect. the correct answer was:\n  {}\n".format(
-            actual_value
-        ))
+        print("incorrect.")
+        if actual_value is not None:
+            print("  the correct answer was: {}".format(
+                actual_value
+            ))
+        if missing:
+            print("  you missed: {}".format(missing))
+        if surplus:
+            print("  you incorrectly had: {}".format(surplus))
+        print()
 
 
 def q_fullband():
@@ -152,13 +183,106 @@ def q_fullband():
     result = query("frequency range of {} band? ".format(band),
                    parse_frequency_range)
     print_eval(
-        result == fullband_index[band],
-        format_frequency_range(fullband_index[band])
+        result == fullband_index[band][0],
+        format_frequency_range(fullband_index[band][0])
     )
 
 
+def q_class_E():
+    "name all class E bands"
+
+    def parse_bandset(s):
+        bands = {
+            band.strip(",").replace(" ", "")
+            for band in s.split()
+        }
+        return bands
+
+    result = query("all bands of class E? ")
+    if result is None:
+        result = set()
+    else:
+        result = parse_bandset(result)
+    bands = classbands_index["E"]
+
+    print_eval(
+        result == bands,
+        None,
+        ", ".join(bands - result),
+        ", ".join(result - bands)
+    )
+
+
+def q_fullband_bw():
+    "band wavelength to max. bandwidth"
+    bands = list(fullband_index.keys())
+    while True:
+        band = RNG.choice(bands)
+        if fullband_index[band][1]:
+            break
+
+    result = query("max. bandwidth of {} band? ".format(band),
+                   parse_frequency)
+    print_eval(
+        result == fullband_index[band][1],
+        format_frequency(fullband_index[band][1], wide=False)
+    )
+
+
+def q_freq_to_fullband():
+    "identify band by frequency"
+    bands = list(fullband_index.keys())
+    band = RNG.choice(bands)
+
+    def mess_with_frequency(f):
+        order = math.floor(math.log(f, 10))
+        factor = 0
+        while factor == 0:
+            factor = RNG.randint(-5, 5)
+        modifier = factor * 10**(order-1)
+        return f + modifier
+
+    (start, end), *_ = fullband_index[band]
+    if RNG.randint(1, 4) != 1:
+        # mess with the numbers
+        n = RNG.randint(1, 3)
+        if n & 1:
+            new_start = mess_with_frequency(start)
+        else:
+            new_start = start
+        if n & 2:
+            new_end = mess_with_frequency(end)
+            if new_end < new_start:
+                new_end = new_start + (end - start)
+        else:
+            new_end = end
+            if new_end < new_start:
+                new_start = new_end + (start - end)
+
+        correct_answer = (start, end) == (new_start, new_end)
+        start, end = new_start, new_end
+        correct_answer = "none" if not correct_answer else band
+    else:
+        correct_answer = band
+
+    result = query(
+        "which band is this: {}? ".format(
+            format_frequency_range((start, end))
+        ),
+        subline="type 'none' if you think this is incorrect"
+    )
+    result = (result or "").replace(" ", "")
+
+    print_eval(
+        result == correct_answer,
+        correct_answer
+    )
+
 QUESTIONS = [
-    q_fullband
+    q_fullband,
+    q_fullband_bw,
+    q_freq_to_fullband,
+    q_class_E
 ]
 
 
